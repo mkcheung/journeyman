fc1145afc3951cc6a0ce9ce8f5a01dcee7088c88
Mars Cheung <mars.kwong.cheung@gmail.com>
2020-07-31 00:40:20 -0700

    rearrange new posts page

--- resources/js/components/NewPost.js
+++ resources/js/components/NewPost.js
@@ -1,185 +1,184 @@
  // resources/assets/js/components/NewPost.js
 
-    import axios from 'axios'
-    import React, { Component } from 'react'
-    import { Editor } from '@tinymce/tinymce-react';
-    import { 
-      Checkbox,
-      FormControl,
-      FormControlLabel,
-      FormLabel,
-      InputLabel,
-      Select,
-      TextareaAutosize
-    } from '@material-ui/core';
+import axios from 'axios'
+import React, { Component } from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import { 
+    Button,
+    Checkbox,
+    Container,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Input,
+    InputLabel,
+    Select,
+    TextField
+} from '@material-ui/core';
 
-    class NewPost extends Component {
-      constructor (props) {
-        super(props)
-        this.state = {
-          title: '',
-          tag:'',
-          slug:'',
-          category_id:'',
-          publish:false,
-          content:'',
-          categories:[],
-          tags:[],
-          errors: []
-        }
-        this.handleFieldChange = this.handleFieldChange.bind(this)
-        this.handleCreateNewPost = this.handleCreateNewPost.bind(this)
-        this.hasErrorFor = this.hasErrorFor.bind(this)
-        this.renderErrorFor = this.renderErrorFor.bind(this)
-      }
+class NewPost extends Component {
 
-      componentDidMount () {
+    state = {
+        loading: true,
+        categories: [],
+        tags:[],
+        errors: [],
+        content:'',
+        title: '',
+        tag:'',
+        slug:'',
+        publish:false,
+        open: false,
+    };
+
+    componentDidMount () {
         axios.get('/api/tags').then(res => {
 
-          let tagOptions = [];
-          let tags = res.data;
+            let tagOptions = [];
+            let tags = res.data;
 
-          tags.forEach(function(tag){
-            let temp = []
-            temp['id'] = tag.id;
-            temp['value'] = tag.title;
-            tagOptions.push(temp);
-          });
-   
-          this.setState( {
-            tags: tagOptions
-          });
+            tags.forEach(function(tag){
+                let temp = []
+                temp['id'] = tag.id;
+                temp['value'] = tag.title;
+                tagOptions.push(temp);
+            });
+
+            this.setState( {
+                tags: tagOptions
+            });
         })
         .catch(error => {
-          return error;
+            return error;
         });
 
         axios.get('/api/categories').then(res => {
 
-          let categoryOptions = [];
-          let categories = res.data;
+            let categoryOptions = [];
+            let categories = res.data;
 
-          categories.forEach(function(category){
-            let temp = []
-            temp['id'] = category.id;
-            temp['value'] = category.title;
-            categoryOptions.push(temp);
-          });
-   
-          this.setState( {
-            categories: categoryOptions
-          });
+            categories.forEach(function(category){
+                let temp = []
+                temp['id'] = category.id;
+                temp['value'] = category.title;
+                categoryOptions.push(temp);
+            });
+
+            this.setState({
+                categories: categoryOptions
+            });
         })
         .catch(error => {
-          return error;
+            return error;
         });
-      }
+    }
 
-      handleFieldChange (event) {
+    handleFieldChange = async (event) => {
         this.setState({
-          [event.target.title]: event.target.value,
-          [event.target.slug]: event.target.value,
-          [event.target.content]: event.target.value,
-          [event.target.category_id]: event.target.value
-        })
-      }
+            [event.target.id]: event.target.value,
+            [event.target.slug]: event.target.value,
+            [event.target.content]: event.target.value,
+            [event.target.category_id]: event.target.value
+        });
+    }
 
-      handleCreateNewPost (event) {
-        event.preventDefault()
+    handleCreateNewPost = async (event) => {
+        event.preventDefault();
 
-        const { history } = this.props
+        const { history } = this.props;
 
         const post = {
-          title: this.state.title,
-          slug: this.state.slug,
-          publish: this.state.publish,
-          content: this.state.content,
-          category_id: this.state.category_id
-        }
+            title: this.state.title,
+            slug: this.state.slug,
+            publish: this.state.publish,
+            content: this.state.content,
+            category_id: this.state.category_id
+        };
 
         axios.post('/api/posts', post)
-          .then(response => {
+        .then(response => {
             // redirect to the homepage
             history.push('/')
-          })
-          .catch(error => {
+        })
+        .catch(error => {
             this.setState({
               errors: error.response.data.errors
             })
-          })
-      }
+        })
+    }
 
-      hasErrorFor (field) {
+    hasErrorFor = (field) => {
         return !!this.state.errors[field]
-      }
+    }
 
-      renderErrorFor (field) {
-        if (this.hasErrorFor(field)) {
-          return (
-            <span className='invalid-feedback'>
-              <strong>{this.state.errors[field][0]}</strong>
-            </span>
-          )
+    renderErrorFor = (field) => {
+        if(this.hasErrorFor(field)) {
+            return (
+                <span className='invalid-feedback'>
+                    <strong>{this.state.errors[field][0]}</strong>
+                </span>
+            )
         }
-      }
+    }
 
-      handleChange = (event) => {
+    handleChange = async (event) => {
         this.setState({
-          ...this.state,
-          [event.target.name]: event.target.value,
+            ...this.state,
+            [event.target.name]: event.target.value,
         });
-      };
+    };
 
-      handleChkboxToggle = (event) => {
+    handleChkboxToggle = async (event) => {
         this.setState({
             ...this.state,
             [event.target.name]: event.target.checked
         });
-      };
+    };
 
-      handleEditorChange = (content, editor) => {
-       console.log('Content was updated:', content);
+    handleEditorChange = async (content, editor) => {
         this.setState({
-          ...this.state,
-          ['content']: content,
+            ...this.state,
+            ['content']: content,
         });
-      }
+    }
 
-      render () {
+    render () {
         return (
-          <div className='container py-4'>
-            <div className='row justify-content-center'>
-              <div className='col-md-6'>
-                <div className='card'>
-                  <div className='card-header'>Create New Post</div>
-                  <div className='card-body'>
+
+            <Container maxWidth="lg">
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <div className='card-header'>Create New Post</div>
+                    </Grid>
                     <form onSubmit={this.handleCreateNewPost}>
-                      <div className='form-group'>
-                        <div>
-                        <label htmlFor='name'>Post title</label>
-                        <input
-                          id='title'
-                          type='text'
-                          classtitle={`form-control ${this.hasErrorFor('title') ? 'is-invalid' : ''}`}
-                          title='title'
-                          value={this.state.title}
-                          onChange={this.handleFieldChange}
-                        />
-                        {this.renderErrorFor('title')}
-                        </div>
-                        <div>
-                        <label htmlFor='name'>Post slug</label>
-                        <input
-                          id='slug'
-                          type='text'
-                          classslug={`form-control ${this.hasErrorFor('slug') ? 'is-invalid' : ''}`}
-                          title='slug'
-                          value={this.state.slug}
-                          onChange={this.handleFieldChange}
-                        />
-                        {this.renderErrorFor('slug')}
-                        </div>
-                        <div>
+                        <Grid item xs={12}>
+                            <div>
+                                <InputLabel htmlFor="name">Title:</InputLabel>
+                                <TextField 
+                                    id="title" 
+                                    title='title' 
+                                    onChange={this.handleFieldChange} 
+                                    aria-describedby="my-helper-text" 
+                                    value={this.state.title}
+                                />
+                                {this.renderErrorFor('title')}
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <div>
+                                <InputLabel htmlFor="name">Slug:</InputLabel>
+                                <TextField 
+                                    id="slug" 
+                                    title='slug' 
+                                    onChange={this.handleFieldChange} 
+                                    aria-describedby="my-helper-text" 
+                                    value={this.state.slug}
+                                />
+                                {this.renderErrorFor('slug')}
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
                             <FormControlLabel
                                 control={
                                     <Checkbox
@@ -191,82 +190,82 @@
                                 }
                                 label="Publish"
                             />
-                        </div>
-                        <div>
-                          <FormControl >
-                            <InputLabel htmlFor="age-native-simple">Tags</InputLabel>
-                            <Select
-                              native
-                              value={this.state.tag}
-                              onChange={this.handleChange}
-                              inputProps={{
-                                name: 'age',
-                                id: 'age-native-simple',
-                              }}
-                            >
-                            <option value='0'></option>
-                            {
-                              Object
-                              .keys(this.state.tags)
-                              .map(key => <option key={key} value = {this.state.tags[key].id}>{this.state.tags[key].value}</option>)
-                            }
-                            </Select>
-                          </FormControl>
-                        </div>
-                        <div>
-                          <FormControl >
-                            <InputLabel htmlFor="age-native-simple">Category</InputLabel>
-                            <Select
-                              native
-                              value={this.state.category_id}
-                              onChange={this.handleChange}
-                              title='category_id'
-                              inputProps={{
-                                name: 'age',
-                                id: 'age-native-simple',
-                              }}
-                            >
-                            <option value='0'></option>
-                            {
-                              Object
-                              .keys(this.state.categories)
-                              .map(key => <option key={key} value = {this.state.categories[key].id}>{this.state.categories[key].value}</option>)
-                            }
-                            </Select>
-                          </FormControl>
-                        </div>
-                        <div>
-                        <label htmlFor='name'>Content</label>
-                        <Editor
-                           initialValue="<p>This is the initial content of the editor</p>"
-                           title='content'
-                           init={{
-                             height: 500,
-                             menubar: false,
-                             plugins: [
-                               'advlist autolink lists link image charmap print preview anchor',
-                               'searchreplace visualblocks code fullscreen',
-                               'insertdatetime media table paste code help wordcount'
-                             ],
-                             toolbar:
-                               'undo redo | formatselect | bold italic backcolor | \
-                               alignleft aligncenter alignright alignjustify | \
-                               bullist numlist outdent indent | removeformat | help'
-                           }}
-                           onEditorChange={this.handleEditorChange}
-                         />
-                        {this.renderErrorFor('content')}
-                        </div>
-                        <button className='btn btn-primary'>Create</button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      }
-    }
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl >
+                                <InputLabel htmlFor="age-native-simple">Tags</InputLabel>
+                                <Select
+                                    native
+                                    value={this.state.tag}
+                                    onChange={this.handleChange}
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'age-native-simple',
+                                    }}
+                                >
+                                <option value='0'></option>
+                                {
+                                    Object
+                                    .keys(this.state.tags)
+                                    .map(key => <option key={key} value = {this.state.tags[key].id}>{this.state.tags[key].value}</option>)
+                                }
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl >
+                                <InputLabel htmlFor="age-native-simple">Category</InputLabel>
+                                <Select
+                                    native
+                                    value={this.state.category_id}
+                                    onChange={this.handleChange}
+                                    title='category_id'
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'age-native-simple',
+                                    }}
+                                >
+                                <option value='0'></option>
+                                {
+                                    Object
+                                    .keys(this.state.categories)
+                                    .map(key => <option key={key} value = {this.state.categories[key].id}>{this.state.categories[key].value}</option>)
+                                }
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Editor
+                                initialValue="<p>This is the initial content of the editor</p>"
+                                title='content'
+                                init={{
+                                height: 500,
+                                menubar: false,
+                                plugins: [
+                                    'advlist autolink lists link image charmap print preview anchor',
+                                    'searchreplace visualblocks code fullscreen',
+                                    'insertdatetime media table paste code help wordcount'
+                                ],
+                                toolbar:
+                                    'undo redo | formatselect | bold italic backcolor | \
+                                    alignleft aligncenter alignright alignjustify | \
+                                    bullist numlist outdent indent | removeformat | help'
+                                }}
+                                onEditorChange={this.handleEditorChange}
+                            />
+                            {this.renderErrorFor('content')}
+                        </Grid>
 
-    export default NewPost
+                        <Grid item xs={6}>
+                            <Button variant="contained" color="primary" >
+                                Create
+                            </Button>
+                        </Grid>
+                    </form>
+                </Grid>
+            </Container>
+        );
+    }
+}
+
+export default NewPost;
