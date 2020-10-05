b573173445e2b61ddc07f267166697b64374f3c6
Mars Cheung <mars.kwong.cheung@gmail.com>
2020-10-05 00:05:16 -0700

    add search option to header

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -6,13 +6,17 @@ import {
 	AppBar,
     Button,
     IconButton,
+    InputBase,
     Link as MUILINK,
     Menu,
     MenuItem,
-    Toolbar
+    Toolbar,
+    Typography
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
+import { fade, withStyles } from '@material-ui/core/styles';
 const navOps = [
     {
         name: 'Dashboard',
@@ -32,6 +36,70 @@ const navOps = [
     },
 ];
 const ITEM_HEIGHT = 48;
+
+const styles = theme => ({
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing(2),
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing(3),
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: '20ch',
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
+});
 
 class Header extends Component {
 	state = {
@@ -56,7 +124,7 @@ class Header extends Component {
 			cursor: 'pointer'
         };
 
-        let { isLoggedIn, handleClick, handleClose, openMenu } = this.props;
+        let { isLoggedIn, handleClick, handleClose, openMenu, classes, theme } = this.props;
 
         return (
         	<AppBar position="static">
@@ -99,9 +167,25 @@ class Header extends Component {
 			                </MenuItem>
 			            }
 		          	</Menu>
+					<Typography variant="h6" noWrap>
+						Phronesis Project
+					</Typography>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+					<InputBase
+						placeholder="Search…"
+						classes={{
+							root: classes.inputRoot,
+							input: classes.inputInput,
+						}}
+						inputProps={{ 'aria-label': 'search' }}
+					/>
+					</div>
         		</Toolbar>
 			</AppBar>
         );
     }
 }
-export default withRouter(Header)
+export default withStyles(styles)(withRouter(Header));
