bd8a5f93e6e88da4c241f3b8e943529960ff2c32
Mars Cheung <mars.kwong.cheung@gmail.com>
2020-11-18 00:01:54 -0800

    create prototype unit test for post controller

---
+++ tests/Feature/PostControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\Http\Controllers\PostController;
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Mockery;
+
+class PostControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetUserPosts()
+    {
+        $userId = 1234;
+        $postGetUserPostsReturn = collect(['test']);
+        // Mock
+        $controller = new PostController();
+        $request = $this->createMock(Request::class);
+        $post = \Mockery::mock('overload:' . Post::class);
+        $post->shouldReceive('where')
+            ->with('user_id', '=', $userId)
+            ->andReturn($post)
+            ->once();
+        $post->shouldReceive('where')
+            ->with('parent', '=', 1)
+            ->andReturn($post)
+            ->once();
+        $post->shouldReceive('with')
+            ->with('user')
+            ->andReturn($post)
+            ->once();
+        $post->shouldReceive('orderBy')
+            ->with('created_at')
+            ->andReturn($post)
+            ->once();
+        $post->shouldReceive('get')
+            ->andReturn($postGetUserPostsReturn)
+            ->once();
+
+        $post->shouldReceive('toJson')
+            ->andReturn($postGetUserPostsReturn)
+            ->once();
+        // Expect
+        $request->expects($this->once())
+            ->method('query')
+            ->with('userId')
+            ->willReturn($userId);
+        // Fire
+        $response = $controller->getUserPosts($request);
+
+        // Assert
+        self::assertJson($response);
+    }
+}
